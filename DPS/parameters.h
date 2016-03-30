a52b87f467bb5948854abb9bf703b04259f9ce58
cimbo92 <alecimbo92@gmail.com>
2016-03-30 19:06:51 +0200

    Now we have different topologies of network

--- DPS/parameters.h
+++ DPS/parameters.h
@@ -9,7 +9,7 @@
 
 const int NTOPIC = 5; //how many topics
 const const_simtime_t RESEND_TIMEOUT = 10.0; //time to re send a self message waiting for the correct timestamp
-const int N_SEND = 50; //how many messages a client should send to a new broker
+const int N_SEND = 10; //how many messages a client should send to a new broker
 const float SUBS_RATIO = 0.1; //the percentage of messages that should be a subscription
 const int MAX_DELAY = 10; //the maximum delay to send a sub/pub msg
 
