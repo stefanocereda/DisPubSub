cda4fd2427b2254d2809019e6ebd61d5d8111206
Stefano Cereda <cereda.ste@gmail.com>
2016-03-30 08:30:19 +0200

    Client show msg, not complete

--- DPS/client.cc
+++ DPS/client.cc
@@ -13,50 +13,51 @@
 
 using namespace omnetpp;
 
-class client : public cSimpleModule
-{
+class client: public cSimpleModule {
 private:
     std::vector<int> ts_vec;
 
     void sendMsg(int topic);
     void sendSub(int topic);
 
- protected:
+    void handleMessageMessage(Message_msg *m);
+    void displayMessage(Message_msg *m);
+
+protected:
     // The following redefined virtual function holds the algorithm.
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
 
- public:
-    client():ts_vec(NTOPIC){}
+public:
+    client() :
+            ts_vec(NTOPIC) {
+    }
 };
 
 // The module class needs to be registered with OMNeT++
 Define_Module(client);
 
-void client::initialize()
-{
-     EV << this->getFullName() << " con id: " << this->getId() << "\n";
+void client::initialize() {
+    EV << this->getFullName() << " con id: " << this->getId() << "\n";
 
-     //Send a random subscription
-     sendSub(intuniform(0,NTOPIC));
+    //Send a random subscription
+    sendSub(intuniform(0, NTOPIC));
 
-     //and send a random message
-     sendMsg(intuniform(0,NTOPIC));
+    //and send a random message
+    sendMsg(intuniform(0, NTOPIC));
 }
 
 //Subscribe to the given topic
-void client::sendSub(int topic)
-{
+void client::sendSub(int topic) {
     Subscribe_msg *msg = new Subscribe_msg("subscribe");
     msg->setSrcId(this->getId());
     msg->setTopic(intuniform(0, NTOPIC));
 
-    send(msg, "gate$o",0);
+    send(msg, "gate$o", 0);
 }
 
 //Send a message for the given topic
-void client::sendMsg(int topic)
-{
+void client::sendMsg(int topic) {
     Message_msg *msg = new Message_msg("message");
     msg->setTopic(topic);
     msg->setTimestamp(++ts_vec[topic]);
@@ -64,17 +65,25 @@ void client::sendMsg(int topic)
     sendDelayed(msg, 10, "gate$o", 0); //TODO spararli fuori a caso
 }
 
-
-
-void client::handleMessage(cMessage *msg)
-{
-    //TODO
+void client::handleMessage(cMessage *msg) {
+    if (strcmp("message", msg->getFullName()) == 0) {
+        handleMessageMessage(dynamic_cast<Message_msg*>(msg));
+    }
 }
 
+//TODO CHECK + KEEP MSG
+void client::handleMessageMessage(Message_msg *m){
+    int topic = m->getTopic();
+    int ts = m->getTimestamp();
 
+    int my_ts = ts_vec[topic];
 
+    if (my_ts == ts+1)
+        displayMessage(m);
+}
 
-
-
-
+//TODO
+void client::displayMessage(Message_msg *m){
+    EV << "client " << this->getId() << " has vector and shows a msg with topic " << m->getTopic() << " and ts " << m->getTimestamp();
+}
 
