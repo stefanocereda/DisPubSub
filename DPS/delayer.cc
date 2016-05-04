3396e66d283ca152bada6655517c7c18cc50fc51
Stefano Cereda <cereda.ste@gmail.com>
2016-05-04 14:43:33 +0200

    Added a delayer in the big Network so that we have to check for consistency in the random simulation

--- DPS/delayer.cc
+++ DPS/delayer.cc
@@ -1,5 +1,6 @@
 #include <omnetpp.h>
 #include "message_m.h"
+#include "parameters.h"
 
 using namespace omnetpp;
 
@@ -12,19 +13,25 @@ protected:
 Define_Module(delayer);
 
 void delayer::handleMessage(cMessage *msg) {
+    const_simtime_t delay = intuniform(MIN_DELAY * 100, MAX_DELAY * 100) / 100;
+
+    //for test
+#if MODE == CONSISTENCY
     //20 22 24 da girare
-    int delay = 0;
+    delay = 0.0;
 
     if (!strcmp(msg->getFullName(), "message")) {
         Message_msg *m = dynamic_cast<Message_msg*>(msg);
 
         if (m->getContent() == '3')
-            delay = 0;
+            delay = 0.0;
         else if (m->getContent() == '2')
-            delay = 25;
+            delay = 25.0;
         else if (m->getContent() == '1')
-            delay = 50;
+            delay = 50.0;
     }
+#endif
+
     if (msg->getArrivalGate()->getIndex() == 0)
         sendDelayed(msg, simTime() + delay, "gate$o", 1);
     else
--- DPS/parameters.h
+++ DPS/parameters.h
@@ -23,12 +23,10 @@
 #if (MODE == CLIENT_LEAVE)||(MODE == BROKER_LEAVE)||(MODE == CONSISTENCY)
     const int NTOPIC = 5; //how many topics
     const int N_SEND = 0; //how many messages a client should send to a new broker
-    const float SUBS_RATIO = 0.1; //the percentage of messages that should be a subscription
+    const float SUBS_RATIO = 0.0; //the percentage of messages that should be a subscription
 
     const const_simtime_t MIN_SUB_DELAY = 0.5; //when to send the subscriptions
     const const_simtime_t MAX_SUB_DELAY = 10.0;
-    const const_simtime_t MIN_PUB_DELAY = 5.0; //when to send the first publish
-    const const_simtime_t MAX_PUB_DELAY = 15.0;
 
     const const_simtime_t RESEND_TIMEOUT = 100.0; //time to re send a self message waiting for the correct timestamp
 
@@ -53,25 +51,28 @@
     const int N_SEND = 5; //how many messages a client should send to a new broker
     const float SUBS_RATIO = 0.6; //the percentage of initial messages that should be a subscription
 
-    const const_simtime_t MIN_SUB_DELAY = 0.5; //when to send the subscriptions
+    const const_simtime_t MIN_SUB_DELAY = 5.0; //when to send the subscriptions
     const const_simtime_t MAX_SUB_DELAY = 10.0;
 
-    const const_simtime_t RESEND_TIMEOUT = 10.0; //time to re send a self message waiting for the correct timestamp
+    const const_simtime_t RESEND_TIMEOUT = 20.0; //time to re send a self message waiting for the correct timestamp
 
-    const float REPLY_PROB = 0.10; //the probability that a client answer to a message
-    const const_simtime_t MIN_REPLY_DELAY = 1.0; //and the time to answer
-    const const_simtime_t MAX_REPLY_DELAY = 5.0;
+    const float REPLY_PROB = 0.085; //the probability that a client answer to a message
+    const const_simtime_t MIN_REPLY_DELAY = 5.0; //and the time to answer
+    const const_simtime_t MAX_REPLY_DELAY = 10.0;
 
-    const float CLIENT_LEAVE_PROBABILITY = 0.2; //prob for client to leave
-    const const_simtime_t MIN_LEAVE_DELAY = 3.0; //when to leave
-    const const_simtime_t MAX_LEAVE_DELAY = 6.0;
-    const const_simtime_t MIN_REJOIN_DELAY = 3.0; //and the additional time to come back
-    const const_simtime_t MAX_REJOIN_DELAY = 6.0;
+    const float CLIENT_LEAVE_PROBABILITY = 0.25; //prob for client to leave
+    const const_simtime_t MIN_LEAVE_DELAY = 30.0; //when to leave
+    const const_simtime_t MAX_LEAVE_DELAY = 60.0;
+    const const_simtime_t MIN_REJOIN_DELAY = 60.0; //and the additional time to come back
+    const const_simtime_t MAX_REJOIN_DELAY = 120.0;
 
-    const float BROKER_LEAVE_PROBABILITY = 0.2;
-    const const_simtime_t MIN_HUB_TIME = 3.0;
-    const const_simtime_t MAX_HUB_TIME = 6.0;
-    const const_simtime_t MIN_BLEAVE_DELAY = 3.0;
-    const const_simtime_t MAX_BLEAVE_DELAY = 6.0;
+    const float BROKER_LEAVE_PROBABILITY = 0.1;
+    const const_simtime_t MIN_HUB_TIME = 30.0;
+    const const_simtime_t MAX_HUB_TIME = 60.0;
+    const const_simtime_t MIN_BLEAVE_DELAY = 60.0;
+    const const_simtime_t MAX_BLEAVE_DELAY = 120.0;
+
+    const const_simtime_t MIN_DELAY = 0.0; //the random delay for the delayers
+    const const_simtime_t MAX_DELAY = 0.5;
 
 #endif
